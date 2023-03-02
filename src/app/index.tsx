33d5ca5f8be2f6f89f0f65e38e8686fec3bf19ce
Yaroslav M <yarmkhlv@gmail.com>
2023-03-02 22:41:54 +0400

    init prj

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
