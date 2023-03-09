63a289e0a65feed3cf7b22a0b0e0071fb3699322
Yaroslav M <yarmkhlv@gmail.com>
2023-03-09 18:55:29 +0400

    gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/test">
         <DAppProvider config={config}>
           <App />
         </DAppProvider>
